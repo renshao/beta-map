acdc214351009756e79ab0eafbaa3e17207607b9
Ren Shao <renshao@gmail.com>
2012-05-03 09:23:11 +1000

    try dark background

--- asset/js/map.js
+++ asset/js/map.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    console.log('document height: ' + $(document).height());
+    $('.map').height($(document).height() - 100);
 	var sydney = new google.maps.LatLng(-33.863093, 151.207731);
 
 	var myOptions = {
@@ -7,7 +9,7 @@ $(document).ready(function() {
 		disableDefaultUI : false,
 		mapTypeId : google.maps.MapTypeId.ROADMAP
 	};
-	new google.maps.Map($("#searchMapCanvas").get(0), myOptions);
+	new google.maps.Map($("#realtimeMapCanvas").get(0), myOptions);
 
     $(document).keydown(function(e){
         if (e.keyCode == 37/*left*/) { 
@@ -19,6 +21,4 @@ $(document).ready(function() {
             return false;
         }
     });
-
-
 });
